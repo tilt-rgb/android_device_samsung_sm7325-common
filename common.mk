a92c9af2b3710badd476fa4abeefffdfdd765241
Bruno Martins <bgcngm@gmail.com>
2025-09-01 12:25:36 +0000

    sm7325-common: Move to Samsung common Touch HAL
    Change-Id: I5c4de6e4956e316600e327b725b863ddf9e63fc1

--- common.mk
+++ common.mk
@@ -228,9 +228,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
-# Include
-$(call soong_config_set,samsungVars,target_specific_header_path,$(LOCAL_PATH)/include)
-
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/sec_touchscreen.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sec_touchscreen.kl
@@ -396,7 +393,7 @@ PRODUCT_PACKAGES += \
 
 # Touch features
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch-service.samsung_sm7325
+    vendor.lineage.touch-service.samsung
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
